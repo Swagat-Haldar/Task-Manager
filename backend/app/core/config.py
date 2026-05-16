303507d6dd742a5db3cdd06c3979f228968cf4ab
Swagat Haldar <haldarswagat@gmail.com>
2026-05-17 00:39:32 +0530

    changed the backend's default state to "Production"

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "YOUR_SUPER_SECRET_KEY"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
-    ENVIRONMENT: str = "development"
+    ENVIRONMENT: str = "production"
     FRONTEND_URL: str = "http://localhost:3000"
     
     @property
